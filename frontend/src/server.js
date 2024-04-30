a2acc6008e547b1a8015ed813eb8db381aeacfa8
Harsh-9000 <harshchaudharydb@gmail.com>
2024-05-01 03:53:17 +0530

    server url changes

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
 export const server = "https://wbd-project.onrender.com/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://wbd-project.onrender.com/";
