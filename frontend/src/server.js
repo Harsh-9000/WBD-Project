ff1a8a42a9c0b559228f999f2a7afbef5ba28211
Harsh-9000 <harshchaudharydb@gmail.com>
2024-05-01 03:31:19 +0530

    render.com deployment

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://wbd-project-server.vercel.app/api/v2";
+export const server = "https://wbd-api.onrender.com/api/v2";
 
 export const backend_url = "http://localhost:8000/";
