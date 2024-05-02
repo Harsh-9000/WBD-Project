6ec54f5bd66e550a509adddda36f88b2087b469b
Harsh-9000 <harshchaudharydb@gmail.com>
2024-05-02 13:17:41 +0530

    shop register

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -114,7 +114,7 @@ router.post(
 
       const firstName = name.split(' ')[0];
       const activationToken = createActivationToken(seller);
-      const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+      const activationUrl = `https://wbd-project-client.vercel.app/activation/${activationToken}`;
 
       try {
         await sendMail({
